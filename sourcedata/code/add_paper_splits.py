be55cdcd5594862f62810a1429c37edf96f1391e
Bru <b.aristimunha@gmail.com>
2026-08-31 12:47:43 +0200

    Preserve BIDS scan timestamps

--- sourcedata/code/add_paper_splits.py
+++ sourcedata/code/add_paper_splits.py
@@ -10,7 +10,7 @@ metadata = pd.read_csv(ROOT / "sourcedata" / "emg2pose_metadata.csv")
 splits = metadata.set_index("filename")[["split", "generalization"]]
 
 for scans_path in ROOT.glob("sub-*/ses-*/*_scans.tsv"):
-    scans = pd.read_csv(scans_path, sep="\t")
+    scans = pd.read_csv(scans_path, sep="\t", keep_default_na=False)
     names = scans.source_file.map(lambda value: Path(value).stem)
     try:
         scans[["split", "generalization"]] = splits.loc[names].to_numpy()
